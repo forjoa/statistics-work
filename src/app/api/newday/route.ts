54e067feb8f764c0b4870d03e11af33c9693013d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-28 21:05:19 +0200

    build: endpoint to add new day

--- src/app/api/newday/route.ts
+++ src/app/api/newday/route.ts
@@ -4,9 +4,24 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
-    const { newDate, newAmount } = body
+    const { newDate, newAmount, id } = body
 
-    
+    const result = await db.execute({
+      sql: 'INSERT INTO delivered_packages(timestamp, quantity, userId) VALUES (?,?,?)',
+      args: [newDate, newAmount, id],
+    })
+
+    if (result.rowsAffected === 0) {
+      return NextResponse.json({
+        success: false,
+        message: 'Something went wrong and the info was not saved',
+      })
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: 'New day uploaded correctly!',
+    })
   } catch {
     return NextResponse.json({
       success: false,

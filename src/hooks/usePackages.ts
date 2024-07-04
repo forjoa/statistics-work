92f8d9aca26bcac964be2e93babfa2a123e5099a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-04 20:45:33 +0200

    build: new function to get data by month

--- src/hooks/usePackages.ts
+++ src/hooks/usePackages.ts
@@ -57,9 +57,45 @@ export const usePackages = (user: User | undefined) => {
     }
   }, [user])
 
+  const fetchDataByMonth = useCallback(async (month: string) => {
+    if (!user) return
+
+    try {
+      const response = await fetch('/api/mypackagesbymonth', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: user.id, month }),
+      })
+
+      if (!response.ok) {
+        toast.error('Failed to fetch data')
+        return
+      }
+
+      const { rows } = await response.json()
+      const packageData = rows.map((r: any) => r.quantity)
+
+      setData((prevData) => ({
+        ...prevData,
+        datasets: [{ ...prevData.datasets[0], data: packageData }],
+      }))
+
+      const subtotal = packageData.reduce(
+        (acc: number, curr: number) => acc + curr,
+        0
+      )
+      setAverage(
+        (subtotal / packageData.length).toFixed(1) as unknown as number
+      )
+      setTotal(subtotal)
+    } catch (error) {
+      toast.error('Error fetching package data: ' + error)
+    }
+  }, [user])
+
   useEffect(() => {
     fetchData()
   }, [fetchData])
 
-  return { data, average, total }
+  return { data, average, total, fetchDataByMonth }
 }

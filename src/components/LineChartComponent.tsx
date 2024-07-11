4ba2bf41dc7b62ff0ffdb0d08ce73a07d5ed5551
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-11 20:13:54 +0200

    refactor: change chart to Rechart

--- src/components/LineChartComponent.tsx
+++ src/components/LineChartComponent.tsx
@@ -1,33 +1,62 @@
-import { Line } from 'react-chartjs-2'
+import { useEffect, useState } from 'react'
 import {
-  Chart,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
   Tooltip,
   Legend,
-} from 'chart.js'
-
-Chart.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-)
+  ResponsiveContainer,
+} from 'recharts'
 
 interface LineChartComponentProps {
   data: any
 }
 
+interface DataI {
+  name: string
+  quantity: number
+}
+
 export default function LineChartComponent({ data }: LineChartComponentProps) {
+  const [dataModified, setDataModified] = useState<DataI[]>([])
+
+  useEffect(() => {
+    const modifiedData = data.datasets[0].data.map((element: any) => {
+      const { userId, day, quantity } = element
+      return {
+        name: (day + '').substring(5),
+        quantity: parseFloat(quantity), 
+      }
+    })
+
+    setDataModified(modifiedData)
+  }, [data])
+
+  console.log(dataModified)
+
   return (
-    <div className='bg-[#EEEEF0] p-4 rounded-lg'>
-      <Line data={data} />
+    <div className='bg-[#EEEEF0] rounded-lg text-black'>
+      {dataModified.length > 0 ? (
+        <ResponsiveContainer width='100%' height={400}>
+          <LineChart data={dataModified}>
+            <CartesianGrid strokeDasharray='3 3' />
+            <XAxis dataKey='name' color='black' />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type='monotone'
+              dataKey='quantity'
+              stroke='#3D63DD'
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      ) : (
+        <p className='text-black'>Loading data...</p>
+      )}
     </div>
   )
 }
